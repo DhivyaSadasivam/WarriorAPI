a9e88a9c7c3f2c9ce4e6d738ed1dd0e4855f9c8f
Dhivya Sadasivam <dhivyas1222@gmail.com>
2019-06-15 22:55:11 +0530

    Committing WarriorAPI

---
+++ WarriorWorld/Controllers/WarriorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WarriorWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarriorController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetBattalion()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> AskWarrior(string Name)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void JoinBattalion([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void ChangeWeapon(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void LeaveBattalion(int id)
+        {
+        }
+    }
+}
